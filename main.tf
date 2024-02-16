3e1f6db34d4605a1fcff0648591a9b1486d60762
DecuBell <105905957+DecuBell@users.noreply.github.com>
2024-02-16 23:27:24 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+# Generate random int
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99000
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}${random_integer.ri.result}"
+  location = var.resource_group_location
+}
+
+resource "azurerm_service_plan" "aasp" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "azlwa" {
+  name                = "${var.app_service_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.aasp.location
+  service_plan_id     = azurerm_service_plan.aasp.id
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.mssql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.mssql.administrator_login};Password=${azurerm_mssql_server.mssql.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+
+}
+
+resource "azurerm_mssql_server" "mssql" {
+  name                         = "${var.sql_server_name}-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "database" {
+  name           = "${var.sql_database_name}-${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.mssql.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_firewall_rule" "firewall" {
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.mssql.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_app_service_source_control" "example" {
+  app_id                 = azurerm_linux_web_app.azlwa.id
+  repo_url               = var.repo_url
+  branch                 = "main"
+  use_manual_integration = true
+}
+
